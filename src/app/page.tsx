7b2d59590feefb37e72b9db7086b04650bee7421
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-29 16:14:52 -0300

    feat: enhance Home component layout with structured content and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 
 export default function Home() {
   return (
-    <div className="font-sans flex">
+    <div className="font-sans flex flex-col h-screen mx-8">
       <Header />
-      {/* <Logo /> */}
+
+      <div className="flex flex-col items-center mt-8">
+        <h1 className="text-2xl font-bold mt-8 text-[#FFE81F]">
+          Welcome to the Star Wars Wiki
+        </h1>
+
+        <p className="text-sm text-gray-500">
+          This is a wiki about all things Star Wars.
+        </p>
+      </div>
+
+      <Footer />
     </div>
   );
 }
