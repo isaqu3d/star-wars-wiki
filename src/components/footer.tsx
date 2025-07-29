821b19b19947b8d65bffcc745c9e7f489b03ff42
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-29 16:11:18 -0300

    feat: add footer component with copyright information

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+export function footer() {
+  return (
+    <footer className="mb-8 flex flex-1 justify-center items-end">
+      <div className="flex justify-center">
+        <p className="text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} Star Wars Wiki
+        </p>
+      </div>
+    </footer>
+  );
+}
