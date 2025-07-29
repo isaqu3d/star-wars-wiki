3e05fc7ed8530326bd79d862e13fe2c84ddc7c31
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-29 16:15:12 -0300

    feat: update Layout component to include a test header and adjust layout structure

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,10 +1,13 @@
+import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import "../globals.css";
 
 export default function Layout() {
   return (
-    <div className="font-sans flex ">
+    <div className="font-sans flex flex-col mx-8">
       <Header />
+
+      <Footer />
     </div>
   );
 }
