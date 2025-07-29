504dc41aefe9dfcccc6df8e0003486c219fd7817
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-29 14:06:23 -0300

    feat: enhance header navigation layout with MenuLink components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { GithubIcon } from "lucide-react";
 import { Logo } from "./logo";
 import { MenuLink } from "./menu-link";
 
@@ -6,8 +7,17 @@ export function Header() {
     <header className="flex justify-between items-center text-white p-4 m-4 w-full">
       <Logo />
 
-      <menu>
+      <menu className="flex space-x-4 items-center">
         <MenuLink href="/characters">Characters</MenuLink>
+        <MenuLink href="/planets">Planets</MenuLink>
+        <MenuLink
+          target="_blank"
+          href="https://github.com/isaqu3d/star-wars-wiki"
+          className="flex items-center gap-2"
+        >
+          GitHub
+          <GithubIcon size={20} />
+        </MenuLink>
       </menu>
     </header>
   );
