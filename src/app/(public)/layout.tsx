ab2c85d8d3d1d99f89ca77b0f9ef37b23fe404ab
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-30 15:15:10 -0300

    feat: implement dynamic page rendering in Layout component

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,11 +1,21 @@
+"use client";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
+import { usePathname } from "next/navigation";
 import "../globals.css";
+import CharactersPage from "./characters/page";
+import PlanetsPage from "./planets/page";
 
 export default function Layout() {
+  const pathname = usePathname();
+  const isActivePage = (page: string) => pathname?.endsWith(page);
+
   return (
     <div className="font-sans flex flex-col mx-8">
       <Header />
+
+      {isActivePage("characters") && <CharactersPage />}
+      {isActivePage("planets") && <PlanetsPage />}
 
       <Footer />
     </div>
