d3eb55e53c92ac73ea04d50047554146e4c2cff9
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-30 14:19:54 -0300

    feat: add CardCharactersProps type definition for character details

---
+++ src/@types/characters.ts
@@ -0,0 +1,11 @@
+export type CardCharactersProps = {
+  name: string;
+  birth_year: string;
+  height: string;
+  gender: string;
+  mass: string;
+  eye_color: string;
+  hair_color: string;
+  skin_color: string;
+  homeworld: string;
+};
