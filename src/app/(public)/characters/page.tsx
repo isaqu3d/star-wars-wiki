9d791fb21c14f5e00fabe21094664679538c8995
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-30 15:15:23 -0300

    feat: implement character listing with search functionality and loading state

--- src/app/(public)/characters/page.tsx
+++ src/app/(public)/characters/page.tsx
@@ -1,7 +1,76 @@
+"use client";
+
+import { CardCharactersProps } from "@/@types/characters";
+import { CardCharacters } from "@/components/card-characters";
+import { Footer } from "@/components/footer";
+import { Input } from "@/components/input";
+import { Loading } from "@/components/loading";
+import api from "@/services/api";
+import { SearchIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+
 export default function CharactersPage() {
+  const [peoples, setPeoples] = useState<CardCharactersProps[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    api.get("people").then(({ data }) => {
+      setPeoples(data);
+      setLoading(false);
+
+      console.log("People data fetched:", data);
+    });
+  }, []);
+
+  const filteredPeoples = peoples.filter((person) =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
-    <div className="font-sans flex">
-      <h1 className="text-2xl font-bold">Characters Page</h1>
+    <div className="font-sans flex flex-col h-screen mx-8">
+      <div className="flex flex-col items-center mt-8">
+        <h1 className="text-2xl font-bold mt-8 text-[#FFE81F]">
+          Welcome to the Star Wars Wiki
+        </h1>
+
+        <p className="text-sm text-gray-500">
+          This is a wiki about all things Star Wars.
+        </p>
+
+        <Input
+          icon={<SearchIcon />}
+          iconPosition="right"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div>
+          {loading ? (
+            <div className="w-full items-center flex justify-center text-center mx-auto flex-1">
+              <Loading />
+            </div>
+          ) : filteredPeoples.length > 0 ? (
+            filteredPeoples.map((people: CardCharactersProps) => (
+              <CardCharacters
+                key={people.name}
+                name={people.name}
+                mass={people.mass}
+                birth_year={people.birth_year}
+                eye_color={people.eye_color}
+                gender={people.gender}
+                height={people.height}
+                hair_color={people.hair_color}
+                skin_color={people.skin_color}
+                homeworld={people.homeworld} // Se tiver esse dado no seu API
+              />
+            ))
+          ) : (
+            <p className="text-gray-500">No characters found.</p>
+          )}
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
