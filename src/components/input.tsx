08b937f7be69797f7ab1c644fd128a27df8ce038
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-30 11:06:52 -0300

    feat: enhance Input component to support customizable className

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,12 +1,23 @@
+import { cn } from "@/utils/cn";
+
 type InputProps = {
   icon?: React.ReactNode;
   iconPosition?: "left" | "right";
+  className?: string;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-export function Input({ icon, iconPosition = "left", ...props }: InputProps) {
+export function Input({
+  icon,
+  iconPosition = "left",
+  className,
+  ...props
+}: InputProps) {
   return (
     <div
-      className={`flex items-center border border-gray-300 rounded-lg w-full max-w-md px-3 py-2 gap-2 focus-within:ring-2 focus-within:ring-blue-500`}
+      className={cn(
+        "flex items-center border border-gray-300 rounded-lg w-full max-w-md px-3 py-2 gap-2 focus-within:ring-2 focus-within:ring-blue-500",
+        className
+      )}
     >
       {icon && iconPosition === "left" && (
         <span className="text-gray-500">{icon}</span>
