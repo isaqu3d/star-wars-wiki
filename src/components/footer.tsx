de6953e6e2150644d3c62aebb3951616c15357c1
Isaque de Sousa <isaque.error404@gmail.com>
2025-08-03 14:02:13 -0300

    feat: add new styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,12 @@ import { cn } from "@/utils/cn";
 export function Footer({ className }: { className?: string }) {
   return (
     <footer
-      className={cn("flex flex-1 justify-center items-end py-8", className)}
+      className={cn(
+        "flex flex-1 h-full justify-center items-end py-8",
+        className
+      )}
     >
-      <div className="flex justify-center">
+      <div className="flex justify-center items-end">
         <p className="text-sm text-gray-500">
           &copy; {new Date().getFullYear()} Star Wars Wiki
         </p>
