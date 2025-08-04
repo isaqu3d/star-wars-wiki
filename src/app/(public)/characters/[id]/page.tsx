21a4d45409e803d48646e9a309b3f876325f9fa3
Isaque de Sousa <isaque.error404@gmail.com>
2025-08-04 11:33:37 -0300

    refactor: add index page

--- src/app/(public)/characters/[id]/page.tsx
+++ src/app/(public)/characters/[id]/page.tsx
@@ -1,12 +1,22 @@
-import { NextRequest } from "next/server";
+import { CardCharactersProps } from "@/@types/types";
+import api from "@/services/api";
 
-export default function Characters(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const id = searchParams.get("id");
+type CharacterPageProps = {
+  params: { id: string };
+};
+
+export default async function Character({ params }: CharacterPageProps) {
+  const { id } = params;
+
+  const res = await api.get(`/people/${id}`);
+  const character: CardCharactersProps = res.data;
 
   return (
-    <div className="font-sans flex">
-      <h1 className="text-2xl font-bold">Characters Page</h1>
+    <div className="p-8">
+      <h1 className="text-2xl font-bold text-[#FFE81F]">{character.name}</h1>
+      <p>Height: {character.height}</p>
+      <p>Mass: {character.mass}</p>
+      <p>Gender: {character.gender}</p>
     </div>
   );
 }
